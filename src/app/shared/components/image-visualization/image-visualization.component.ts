35dce21fc376f601fa44a9de815a113fae96d1c3
André <andrevera.pe@gmail.com>
2024-10-11 11:58:49 -0500

    feat: change prompt to replace background

--- src/app/shared/components/image-visualization/image-visualization.component.ts
+++ src/app/shared/components/image-visualization/image-visualization.component.ts
@@ -98,70 +98,17 @@ export class ImageVisualizationComponent implements OnInit {
           .resize(scale().width(1000))
           .delivery(quality(auto()))
           .delivery(format(auto()))
-          //.effect(backgroundRemoval()) //quito el fondo
           .effect(generativeBackgroundReplace().prompt(newFil)) //reemplkaza el fondo
-          //.effect(generativeBackgroundReplace().prompt('put some spiders'))
-          //.effect(generativeRecolor('face', 'red')) //cambia el color de algo
-          //.effect(generativeRecolor('eyes', 'red'))
-          .effect(generativeReplace().from('shirt').to('vampire jacket'))
-          .effect(
-            generativeRecolor(['eye', 'hair'], '#331c5d').detectMultiple()
-          ) //pinta algo
-          /* .overlay(
-            source(image('hackathon-images/opnfyevdqmrivkgg4xqeo')).position(
-              new Position()
-                .gravity(compass('south_east'))
-                .offsetX(20)
-                .offsetY(20)
-            )
-          ); */
-        /* .resize(
-            pad()
-              .width(800)
-              .height(800)
-              .background(generativeFill().prompt('mug of coffee and cookies'))
-          ) */
-        //.resize(scale().height(150)); //agregar objeto a la imagen
-        //.resize(pad().background(generativeFill().prompt(newFil)))
-        //.effect(generativeRemove().prompt('eyes').detectMultiple())
-        //.effect(outline().width(15).blurLevel(200).color('#331c5d'));
-        //.effect(generativeReplace().from('eyes').to('scars'))
-        /* .effect(
-            generativeReplace()
-              .from('shirt')
-              .to('cable knit sweater')
-              .preserveGeometry()
-          ) */
-        //.effect(generativeRemove().prompt('eyes')); //remueve algo
-        //.effect(generativeRecolor('device', '#EA672A').detectMultiple());//cambia el color de todo lo que encuentre en este ejemplo dispositivos
-        //.effect(generativeRecolor(['device', hair], '#EA672A').detectMultiple());//cambia el color de todo lo que encuentre en este ejemplo dispositivos y pelo
-        //.effect(generativeRecolor('eyes', 'red'))
-        //.effect(generativeRecolor('eye', 'blue').detectMultiple());
-        //.effect(extract().prompt('camera', 'glasses', 'plant')); extraer
-        //.effect(enhance()); //mejorar imagen
-        //.adjust(sharpen().strength(150)); // mejorar tmb
-        /* .resize(
-            auto()
-              .width(880)
-              .height(940)
-              .gravity(autoGravity())
-          ); */
-        /* .resize(
-            pad()
-              .aspectRatio(ar1X1())
-              .gravity(compass("center"))
-              .background(generativeFill())
-          ); */
-
+          .effect(generativeRecolor(['hair'], '#331c5d').detectMultiple()); //pinta algo
         this.isLoading = true;
         //console.log('cloudinaryImage', this.cloudinaryImage);
-        console.log('cloudinaryImage.toURL()', this.cloudinaryImage.toURL());
+        //console.log('cloudinaryImage.toURL()', this.cloudinaryImage.toURL());
         this.transformedImage = this.cloudinaryImage.toURL();
       }
     });
   }
   onLoadTransformedImage() {
-    console.log('onLoadTransformedImage', this.transformedImage);
+    //console.log('onLoadTransformedImage', this.transformedImage);
     this.isLoading = false;
   }
   onErrorLoadImage() {
@@ -176,4 +123,55 @@ export class ImageVisualizationComponent implements OnInit {
 /**
  * const tagsObject = result.info.info.detection.object_detection.data.unidet.tags;
         const tagNames = Object.keys(tagsObject);
+        
  */
+/* .overlay(
+            source(image('hackathon-images/opnfyevdqmrivkgg4xqeo')).position(
+              new Position()
+                .gravity(compass('south_east'))
+                .offsetX(20)
+                .offsetY(20)
+            )
+          ); */
+/* .resize(
+            pad()
+              .width(800)
+              .height(800)
+              .background(generativeFill().prompt('mug of coffee and cookies'))
+          ) */
+//.effect(backgroundRemoval()) //quito el fondo
+//.effect(generativeBackgroundReplace().prompt('put some spiders'))
+//.effect(generativeRecolor('face', 'red')) //cambia el color de algo
+//.effect(generativeRecolor('eyes', 'red'))
+//.effect(generativeReplace().from('shirt').to('vampire jacket'))
+//.resize(scale().height(150)); //agregar objeto a la imagen
+//.resize(pad().background(generativeFill().prompt(newFil)))
+//.effect(generativeRemove().prompt('eyes').detectMultiple())
+//.effect(outline().width(15).blurLevel(200).color('#331c5d'));
+//.effect(generativeReplace().from('eyes').to('scars'))
+/* .effect(
+            generativeReplace()
+              .from('shirt')
+              .to('cable knit sweater')
+              .preserveGeometry()
+          ) */
+//.effect(generativeRemove().prompt('eyes')); //remueve algo
+//.effect(generativeRecolor('device', '#EA672A').detectMultiple());//cambia el color de todo lo que encuentre en este ejemplo dispositivos
+//.effect(generativeRecolor(['device', hair], '#EA672A').detectMultiple());//cambia el color de todo lo que encuentre en este ejemplo dispositivos y pelo
+//.effect(generativeRecolor('eyes', 'red'))
+//.effect(generativeRecolor('eye', 'blue').detectMultiple());
+//.effect(extract().prompt('camera', 'glasses', 'plant')); extraer
+//.effect(enhance()); //mejorar imagen
+//.adjust(sharpen().strength(150)); // mejorar tmb
+/* .resize(
+            auto()
+              .width(880)
+              .height(940)
+              .gravity(autoGravity())
+          ); */
+/* .resize(
+            pad()
+              .aspectRatio(ar1X1())
+              .gravity(compass("center"))
+              .background(generativeFill())
+          ); */
--- src/app/shared/constants/available-effects.ts
+++ src/app/shared/constants/available-effects.ts
@@ -1,37 +1,37 @@
 export const AVAILABLE_EFFECTS = [
   {
     id: 'magic',
-    prompt: 'scary background with a witch',
+    prompt: 'replace background with a scary witch',
     description: 'Magia',
     image: 'icons8-magic-100.png',
   },
   {
     id: 'moon',
-    prompt: 'scary background with a moon',
+    prompt: 'replace background with a scary moon',
     description: 'Luna',
     image: 'icons8-moon-100.png',
   },
   {
     id: 'darkness',
-    prompt: 'scary background with a darkness',
+    prompt: 'replace background with a scary darkness',
     description: 'Oscuridad',
     image: 'icons8-night-96.png',
   },
   {
     id: 'zombie',
-    prompt: 'scary background with a zombie',
+    prompt: 'replace background with a scary zombie',
     description: 'Zombie',
     image: 'icons8-zombie-80.png',
   },
   {
     id: 'ghosts',
-    prompt: 'scary background with a ghosts',
+    prompt: 'replace background with a scary ghosts',
     description: 'Fantasma',
     image: 'icons8-ghost-80.png',
   },
   {
     id: 'skull',
-    prompt: 'scary background with a skull',
+    prompt: 'replace background with a scary skull',
     description: 'Calavera',
     image: 'icons8-skull-100.png',
   },
